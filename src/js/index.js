17bc626c46fbbff144e17d71d52c8ebf87531bae
Jen Hail <jen@mightily.com>
2021-01-18 16:09:35 -0500

    Adding animations to the hero w/ Splitting + GSAP

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 import '../styles/main.scss';
 
 import vue from './components/vue';
+import headlines from './components/animations';
 
 document.addEventListener('DOMContentLoaded', function () {
 	vue();
+	Splitting();
+	headlines();
 });
