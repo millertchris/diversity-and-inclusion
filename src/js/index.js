69097d000285b76e800c7e3c9a92b840e12af937
Jen Hail <jen@mightily.com>
2021-01-18 16:19:58 -0500

    Adding the simple fade-in JS trick

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,15 @@ import vue from './components/vue';
 import headlines from './components/animations';
 
 document.addEventListener('DOMContentLoaded', function () {
+	function doStuff(callback) {
+		// do all app scripts here...
+		callback();
+	}
+
+	doStuff(function () {
+		document.body.className = 'visible';
+	});
+
 	vue();
 	Splitting();
 	headlines();
